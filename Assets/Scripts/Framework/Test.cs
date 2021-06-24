d952e510b7fe1879685cb1ce65201ce8b0eaef6c
Charlotte_M3 <1049223245@qq.com>
2021-06-24 15:58:13 +0800

    Re:

---
+++ Assets/Scripts/Framework/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        AssetBundleCreateRequest request = AssetBundle.LoadFromFileAsync(Application.streamingAssetsPath + "/ui/resources/bg_1.jpg.ab"); //加载资源AB包
+        yield return request;
+
+        AssetBundleCreateRequest request1 = AssetBundle.LoadFromFileAsync(Application.streamingAssetsPath + "/ui/resources/character_1.png.ab"); //加载资源AB包
+        yield return request1;
+
+        AssetBundleCreateRequest request2 = AssetBundle.LoadFromFileAsync(Application.streamingAssetsPath + "/ui/prefabs/character.prefab.ab"); //加载预设体AB包
+        yield return request2;
+
+        AssetBundleRequest bundleRequest = request2.assetBundle.LoadAssetAsync("Assets/BuilResources/UI/prefabs/Character.prefab"); //加载预设资源
+        yield return bundleRequest;
+        //实例化
+        GameObject go =Instantiate(bundleRequest.asset) as GameObject;
+        go.transform.SetParent(this.transform);
+        go.SetActive(true);
+        go.transform.localPosition = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Framework/Util/PathUtil.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+public class PathUtil
+{
+    ///<summary> Assets根目录 </summary>
+    public static readonly string AssetsPath = Application.dataPath;
+    ///<summary> Build输入目录 </summary>
+    public static readonly string BuildResourcesPath = AssetsPath + "/BuilResources/";
+    ///<summary> Build输出目录 </summary>
+    public static readonly string BuildeOutPath = Application.streamingAssetsPath;
+    ///<summary> Build资源路径 </summary>
+    public static string BundleResourcePath {  get { return Application.streamingAssetsPath;  } }
+
+    /// <summary>
+    /// 获取Unity相对路径
+    /// </summary>
+    /// <param name="path">路径</param>
+    /// <returns></returns>
+    public static string GetUnityPath(string path)
+    {
+        if (string.IsNullOrEmpty(path))
+            return string.Empty;
+        return path.Substring(path.IndexOf("Assets"));
+    }
+
+    /// <summary>
+    /// 获取标准路径
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static string GetStandardPath(string path)
+    {
+        if (string.IsNullOrEmpty(path))
+            return string.Empty;
+        return path.Trim().Replace("\\", "/");
+    }
+
+    ///<summary> 获取Lua路径 </summary>
+    public static string GetLuaPath(string name)
+    {
+        return string.Format("Assets/BuilResources/LuaScripts/{0}.bytes", name);
+    }
+
+    ///<summary> 获取UI路径 </summary>
+    public static string GetUIPath(string name)
+    {
+        return string.Format("Assets/BuilResources/UI/prefabs/{0}.prefab", name);
+    }
+
+    ///<summary> 获取场景路径 </summary>
+    public static string GetScenePath(string name)
+    {
+        return string.Format("Assets/BuilResources/Scenes/{0}.unity", name);
+    }
+
+    ///<summary> 获取特效路径 </summary>
+    public static string GetEffectPath(string name)
+    {
+        return string.Format("Assets/BuilResources/Effect/prefabs/{0}.prefab}", name);
+    }
+
+    ///<summary> 获取音效路径 </summary>
+    public static string GetMusicPath(string name)
+    {
+        return string.Format("Assets/BuilResources/Audio/Music/{0}", name);
+    }
+
+    ///<summary> 获取音乐路径 </summary>
+    public static string GetSoundPath(string name)
+    {
+        return string.Format("Assets/BuilResources/Audio/Sound/{0}", name);
+    }
+
+    ///<summary> 获取贴图路径 </summary>
+    public static string GetSpritePath(string name)
+    {
+        return string.Format("Assets/BuilResources/Sprite/{0}", name);
+    }
+
+}
